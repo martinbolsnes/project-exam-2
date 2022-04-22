70c2c8b4b8bb60f89e77dc05636c1f6cfb77515b
martinbolsnes <martinbolsnes@icloud.com>
2022-04-22 12:31:22 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontFamily: {
+      serif: ['Prompt', 'sans-serif'],
+      serif2: ['Ubuntu', 'sans-serif'],
+      display: ['Changa One'],
+    },
+    colors: {
+      'blue-0': '#E3F2FD',
+      'blue-1': '#BBDEFB',
+      'blue-2': '#90CAF9',
+      'blue-3': '#64B5F6',
+      'blue-4': '#42A5F5',
+      'blue-5': '#2196F3',
+      'blue-6': '#1E88E5',
+      'blue-7': '#1976D2',
+      'blue-8': '#1565C0',
+      'blue-9': '#00D47A1',
+      darkBlue: '#07192C',
+      linkHoverColor: '#E8E8E8',
+      bgColor: '#EEEEEE',
+      yellow: '#EAD200',
+      footerColor: '#DFDFDF',
+      black: '#000000',
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
