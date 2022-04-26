3f222a908f91cf880fbe30e303f2d6d0335a9303
martinbolsnes <martinbolsnes@icloud.com>
2022-04-26 13:51:17 +0200

    update

--- components/icons/outdoors.js
+++ components/icons/outdoors.js
@@ -5,18 +5,20 @@ import styles from '../../styles/Home.module.css';
 const OutdoorsIcon = () => {
   return (
     <div className={styles.outdoorsDiv}>
-      <Tooltip
-        label='Outdoors'
-        position='right'
-        withArrow
-        transitionDuration={220}
-        transition='fade'
-        gutter={-20}
-      >
-        <div className={styles.heroIconsBg}>
-          <Tent size={40} color='#1565C0' strokeWidth={1} />
-        </div>
-      </Tooltip>
+      <a>
+        <Tooltip
+          label='Outdoors'
+          position='right'
+          withArrow
+          transitionDuration={220}
+          transition='fade'
+          gutter={-20}
+        >
+          <div className={styles.heroIconsBg}>
+            <Tent size={40} color='#1565C0' strokeWidth={1} />
+          </div>
+        </Tooltip>
+      </a>
     </div>
   );
 };
