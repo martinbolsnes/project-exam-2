3ccdf71b02338dea4d3368d88f1aece47454b363
martinbolsnes <martinbolsnes@icloud.com>
2022-04-27 12:28:29 +0200

    update slider

--- components/main/popularSlider.js
+++ components/main/popularSlider.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 import { Star } from 'tabler-icons-react';
 import Image from 'next/image';
 import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import styles from '../../styles/Home.module.css';
 
@@ -13,6 +15,32 @@ const settings = {
   slidesToShow: 3,
   slidesToScroll: 1,
   arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 };
 
 export default function PopularSlider() {
--- components/main/recommendedSlider.js
+++ components/main/recommendedSlider.js
@@ -3,16 +3,44 @@ import { useState, useEffect } from 'react';
 import { Star } from 'tabler-icons-react';
 import Image from 'next/image';
 import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import styles from '../../styles/Home.module.css';
 
 const settings = {
   dots: true,
-  infinite: false,
   speed: 500,
   slidesToShow: 3,
   slidesToScroll: 1,
   arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+      },
+    },
+  ],
 };
 
 export default function RecommendedSlider() {
