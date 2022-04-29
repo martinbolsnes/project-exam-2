7496f16e5f513e26f36279c7b3e3301d307929de
martinbolsnes <martinbolsnes@icloud.com>
2022-04-29 14:39:58 +0200

    made search btn comp

---
+++ components/buttons/searchButton.js
@@ -0,0 +1,12 @@
+export default function SearchButton() {
+  return (
+    <div className='rounded-md shadow'>
+      <a
+        href=''
+        className='flex items-center py-2 px-4 ml-2 justify-center text-base font-medium font-serif2 rounded-md text-white bg-darkBlue hover:bg-darkBlue '
+      >
+        Search
+      </a>
+    </div>
+  );
+}
