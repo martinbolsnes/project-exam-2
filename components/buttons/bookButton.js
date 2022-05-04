0e8fc5014021988a1002cd7e46187fdc950bdfe2
martinbolsnes <martinbolsnes@icloud.com>
2022-05-04 16:48:01 +0200

    added cursor

--- components/buttons/bookButton.js
+++ components/buttons/bookButton.js
@@ -1,6 +1,6 @@
 export default function BookButton() {
   return (
-    <div className='rounded-md shadow'>
+    <div className='rounded-md shadow cursor-pointer'>
       <p
         href=''
         className='flex items-center justify-center px-16 py-3 text-xl font-medium font-serif2 rounded-md text-white bg-blue-5 hover:bg-darkBlue md:px-16 lg:px-24'
