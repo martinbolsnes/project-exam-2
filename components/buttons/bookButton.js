c517a6d39cdbddaa6c918a711df3ca9e75103c72
martinbolsnes <martinbolsnes@icloud.com>
2022-05-04 12:55:41 +0200

    made booking btn

---
+++ components/buttons/bookButton.js
@@ -0,0 +1,12 @@
+export default function BookButton() {
+  return (
+    <div className='rounded-md shadow'>
+      <p
+        href=''
+        className='flex items-center justify-center px-24 py-3 text-xl font-medium font-serif2 rounded-md text-white bg-blue-5 hover:bg-darkBlue'
+      >
+        Book now
+      </p>
+    </div>
+  );
+}
