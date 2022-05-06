abfa723f9ef24790e3e5fd01aedfdc1ee2c3f053
martinbolsnes <martinbolsnes@icloud.com>
2022-05-06 13:55:38 +0200

    added props

--- components/buttons/bookButton.js
+++ components/buttons/bookButton.js
@@ -1,11 +1,11 @@
-export default function BookButton() {
+export default function BookButton(props) {
   return (
     <div className='rounded-md shadow cursor-pointer'>
       <p
         href=''
         className='flex items-center justify-center px-16 py-3 text-xl font-medium font-serif2 rounded-md text-white bg-blue-5 hover:bg-darkBlue md:px-16 lg:px-24'
       >
-        Book now
+        {props.name}
       </p>
     </div>
   );
