34cf34cddaea34917873376621b585fea7541cf4
martinbolsnes <martinbolsnes@icloud.com>
2022-05-06 13:01:52 +0200

    made modalButton comp

---
+++ components/buttons/modalBookButton.js
@@ -0,0 +1,12 @@
+export default function ModalBookButton() {
+  return (
+    <div className='rounded-md shadow cursor-pointer'>
+      <p
+        href=''
+        className='flex items-center justify-center px-8 py-3 text-xl font-medium font-serif2 rounded-md text-white bg-blue-5 hover:bg-darkBlue md:px-16 lg:px-24'
+      >
+        Book now
+      </p>
+    </div>
+  );
+}
