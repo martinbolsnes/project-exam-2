327150336050bcfbdd3b28c39b1a691e2a78c342
martinbolsnes <martinbolsnes@icloud.com>
2022-05-10 15:39:17 +0200

    edit margin

--- components/buttons/searchButton.js
+++ components/buttons/searchButton.js
@@ -1,9 +1,9 @@
 export default function SearchButton() {
   return (
-    <div className='rounded-md shadow'>
+    <div className='rounded-md shadow ml-2'>
       <a
         href=''
-        className='flex items-center py-2 px-4 ml-2 justify-center text-base font-medium font-serif2 rounded-md text-white bg-darkBlue hover:bg-darkBlue '
+        className='flex items-center py-2 px-4 justify-center text-base font-medium font-serif2 rounded-md text-white bg-darkBlue hover:bg-darkBlue '
       >
         Search
       </a>
