2492745e69445c8170993fcdce01fe447bcfa8c3
martinbolsnes <martinbolsnes@icloud.com>
2022-05-10 14:31:47 +0200

    made submenu comp admin page

---
+++ components/admin/submenu.js
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const SubmenuLinks = [
+  { name: 'Accommodations', href: '/admin' },
+  { name: 'Messages', href: '/admin/messages' },
+  { name: 'Enquieries', href: '/admin/enquieries' },
+];
+
+export default function Submenu() {
+  const router = useRouter();
+  return (
+    <div className='flex space-x-4 mt-4 '>
+      {SubmenuLinks.map((item) => (
+        <Link key={item.name} href={item.href}>
+          <a
+            href={item.href}
+            className={`text-black font-serif text-lg hover:text-blue-2 ${
+              router.pathname === item.href ? 'text-blue-3 font-bold' : ''
+            }`}
+          >
+            {item.name}
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+}
