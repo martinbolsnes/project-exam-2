f7c852b23d4e275334968b701748738092d18457
martinbolsnes <martinbolsnes@icloud.com>
2022-05-24 17:08:26 +0200

    changed search button

--- components/buttons/searchButton.js
+++ components/buttons/searchButton.js
@@ -1,12 +1,11 @@
+import { Search } from 'tabler-icons-react';
+
 export default function SearchButton() {
   return (
     <div className='rounded-md shadow ml-2'>
-      <a
-        href=''
-        className='flex items-center py-2 px-4 justify-center text-base font-medium font-serif2 rounded-md text-white bg-darkBlue hover:bg-darkBlue '
-      >
-        Search
-      </a>
+      <div className='flex items-center py-2 px-4 justify-center text-base font-medium font-serif2 rounded-md text-white bg-darkBlue hover:bg-darkBlue '>
+        <Search />
+      </div>
     </div>
   );
 }
